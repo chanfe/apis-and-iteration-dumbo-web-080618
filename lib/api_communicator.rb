0952a19814f7d457d84e002740404d524770ef53
github username <github email address>
2018-08-07 13:22:57 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,10 +6,15 @@ def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+  movie_array = []
+  response_hash["results"].each do |movies_in|
+    if character == movies_in["name"].downcase
+      movie_array = movies_in["films"]
+    end
+  end
+  movie_array
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
-
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -24,6 +29,15 @@ end
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  if films_hash.length > 0
+    for i in 0..films_hash.length-1
+      response_string = RestClient.get(films_hash[i])
+      response_hash = JSON.parse(response_string)
+      puts "#{i+1}. #{response_hash["title"]}"
+    end
+  else
+    puts "There is no character with that name"
+  end
 end
 
 def show_character_movies(character)
